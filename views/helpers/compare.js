179808490a6e70a9afa1d08644d678b393c138af
SteffanCal <callaghan.steffan@gmail.com>
2023-10-25 19:57:09 +0800

    fifth commit

--- views/helpers/compare.js
+++ views/helpers/compare.js
@@ -4,7 +4,7 @@ module.exports = function (handlebars) {
   return function (arg1, arg2, options) {
     console.log(arg2)
     return arg1.map((e) => {
-      return e.correct_answer == arg2[correctanswers.indexOf(e)]
+      return e.correct_answer == arg2[arg1.indexOf(e)]
         ? options.fn(e)
         : options.inverse(e);
     });
